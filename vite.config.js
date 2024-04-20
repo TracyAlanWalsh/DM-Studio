b727cadada8420fc49921429f113f639cd94d2a0
Tracy Walsh <163774083+TracyAlanWalsh@users.noreply.github.com>
2024-04-20 11:17:18 -0400

    Your commit message

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import reactRefresh from '@vitejs/plugin-react-refresh';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/DM-Studio/',
-  plugins: [react()],
-})
+  plugins: [reactRefresh()],
+  base: '/DM-Studio/', // Update the base path
+  server: {
+    port: 5173, // or any other port you'd like
+    host: '0.0.0.0', // This will allow you to access the server from any device in the network
+  },
+});
